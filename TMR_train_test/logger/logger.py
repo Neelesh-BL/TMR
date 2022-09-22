5ad5f9706b676a4dda801059cf855d65a880f5ea
hp <neelesh.rawat@bridgelabz.com>
2022-09-23 02:08:29 +0530

    [Neelesh] Add: logger file added

---
+++ TMR_train_test/logger/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+def logger_init(name):
+    logger = logging.getLogger('name')
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s:%(filename)s:%(levelname)s:%(lineno)d:%(message)s')
+
+    if not os.path.exists('logger'):
+        os.makedirs('logger')
+    else:
+        file_handler = logging.FileHandler('logger/train_test.log')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
